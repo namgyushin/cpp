2a10a19e9565b54c4a7c7174b0c95f5c85de26c6
namgyushin <namgyushin@naver.com>
2024-08-28 09:48:59 +0900

    d

--- string/string.cpp
+++ string/string.cpp
@@ -2,6 +2,11 @@
 #include <cstring>
 #include "string.h"
 
+std::ostream& operator<<(std::ostream& out, const String& rhs)
+{
+    return out << rhs.str_;
+}
+
 String::String()
 : str_(new char[1]), len_(0)
 {
@@ -27,4 +32,44 @@ String::String(const String& rhs)
 String::~String()
 {
     delete [] str_;
+}
+
+String& String::operator=(const String& rhs)
+{
+    if (this != &rhs)   //self assignment test!     ex) s1 = s1;
+    {
+        delete [] str_;
+        str_= new char[rhs.len_];
+        assert(str_);
+        strcpy(str_, rhs.str_);
+        len_ = rhs.len_;
+    }
+
+    return *this;
+}
+
+bool String::operator==(const String& rhs) const
+{
+    return strcmp(str_, rhs.str_) == 0;
+}
+
+const String String::operator+(const String& rhs) const
+{
+    //char buf[len_ + rhs.len_ +1];
+    char *buf = new char[len_ + rhs.len_ + 1];
+    strcpy(buf, str_);
+    strcat(buf, rhs.str_);
+
+    String result(buf);
+    return result;
+}
+
+const char *String::c_str() const
+{
+    return str_;
+}
+
+int String::length() const
+{
+    return len_;
 }
--- string/string.h
+++ string/string.h
@@ -1,8 +1,11 @@
 #ifndef STRING_H
 #define STRING_H
+#include <iostream>
 
 class String
 {
+    friend std::ostream& operator<<(std::ostream& out, const String& rhs);
+
     private:
         char *str_;
         int len_;
@@ -16,6 +19,14 @@ class String
         // String* operator&() { return this; }
         // const String* operator&() { return this; }
 
-        // String& operator=(const String& ths) { /* memberwise copy */ return *this }
+        String& operator=(const String& ths);        // 컴파일러 shallow copy X --> deep copy!
+
+
+        bool operator==(const String& rhs) const;
+
+        const String operator+(const String& rhs) const;
+
+        const char *c_str() const;
+        int length() const;
 };
 #endif
